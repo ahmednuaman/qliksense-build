567a009d23e7b8fc1535458be1e4b603896b98f1
Ahmed Nuaman <ahmed@ahmednuaman.com>
2017-10-23 19:35:40 +0100

    fixed

--- index.js
+++ index.js
@@ -2,49 +2,53 @@
 
 const archiver = require('archiver')
 const fs = require('fs')
+const makeDir = require('make-dir')
 const path = require('path')
 
 const BUILD_DIR = './build'
 const CWD = process.cwd()
 const NAME = process.env.NAME
 
-const output = fs.createWriteStream(path.resolve(CWD, BUILD_DIR, `${NAME}.zip`))
-const archive = archiver('zip', {
-  zlib: {
-    level: 9
-  }
-})
-
-output.on('close', () => {
-  console.log(`${archive.pointer()} total bytes`)
-  console.log('archiver has been finalized and the output file descriptor has closed.')
-})
-
-output.on('end', () => {
-  console.log('Data has been drained')
-})
-
-archive.on('warning', (err) => {
-  throw err
-})
-
-archive.on('error', (err) => {
-  throw err
-})
-
-const files = ['grid.html', 'preview.png'].concat(['css', 'html', 'js', 'qext'].map((ext) => `${NAME}.${ext}`))
-const wbfolder =
-  files
-    .map((name) => {
-      archive.append(fs.createReadStream(path.resolve(CWD, name)), { name })
-
-      return name
+makeDir(path.resolve(CWD, BUILD_DIR))
+  .then(() => {
+    const output = fs.createWriteStream(path.resolve(CWD, BUILD_DIR, `${NAME}.zip`))
+    const archive = archiver('zip', {
+      zlib: {
+        level: 9
+      }
     })
-    .join(';\n')
 
-archiver.file(wbfolder, {
-  name: 'wbfolder.wbl'
-})
+    output.on('close', () => {
+      console.log(`${archive.pointer()} total bytes`)
+      console.log('archiver has been finalized and the output file descriptor has closed.')
+    })
 
-archive.pipe(output)
-archive.finalize()
+    output.on('end', () => {
+      console.log('Data has been drained')
+    })
+
+    archive.on('warning', (err) => {
+      throw err
+    })
+
+    archive.on('error', (err) => {
+      throw err
+    })
+
+    const files = ['grid.html', 'preview.png'].concat(['css', 'html', 'js', 'qext'].map((ext) => `${NAME}.${ext}`))
+    const wbfolder =
+      files
+        .map((name) => {
+          archive.append(fs.createReadStream(path.resolve(CWD, name)), { name })
+
+          return name
+        })
+        .join(';\n')
+
+    archive.append(wbfolder, {
+      name: 'wbfolder.wbl'
+    })
+
+    archive.pipe(output)
+    archive.finalize()
+  })
